4414f277665f9d285314bd44ad4ac097d1af36da
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-05-25 03:04:04 -0400

    build: add heroku infra config

---
+++ deployments/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "pg" {
+  }
+
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "heroku_app" "gopin" {
+  name = "gopin"
+  region = "us"
+  stack = "container"
+}
+
+resource "heroku_addon" "jawsdb" {
+  app_id = heroku_app.gopin.id
+  plan   = "jawsdb:kitefin"
+}
+
+output "gopin_url" {
+  value = "https://${heroku_app.gopin.name}.herokuapp.com"
+}
